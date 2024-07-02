0962b660e1475c8ef7f3687d11b80636988209af
Sanjay Bhaambhu <bhaambhu@gmail.com>
2024-07-02 17:26:07 +0530

    springdocs fix

--- src/main/java/com/bhaambhu/scalevote/ScalevoteApplication.java
+++ src/main/java/com/bhaambhu/scalevote/ScalevoteApplication.java
@@ -3,11 +3,15 @@ package com.bhaambhu.scalevote;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class ScalevoteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScalevoteApplication.class, args);
 	}
-	
+
 }
