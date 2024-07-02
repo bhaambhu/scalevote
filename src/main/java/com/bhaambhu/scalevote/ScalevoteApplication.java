27f610fafc508bd7fc4068875634df68eed3aa29
Sanjay Bhaambhu <bhaambhu@gmail.com>
2024-07-02 20:31:30 +0530

    Updated CORS policy

--- src/main/java/com/bhaambhu/scalevote/ScalevoteApplication.java
+++ src/main/java/com/bhaambhu/scalevote/ScalevoteApplication.java
@@ -2,16 +2,31 @@ package com.bhaambhu.scalevote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 
-@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL") })
 @SpringBootApplication
 public class ScalevoteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScalevoteApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+			}
+		};
 	}
 
 }
